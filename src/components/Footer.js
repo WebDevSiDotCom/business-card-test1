1621c077431864cb154e80a482e7e179179e8406
WebDevSiDotCom <96456502+WebDevSiDotCom@users.noreply.github.com>
2022-07-23 23:57:25 -0300

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,8 @@
-import React from 'react'
-
 export default function Footer() {
     return (
         <div className="footer">
-            <a href="https://github.com/WebDevSiDotCom" target="_blank" className="social-btn"><i className="fa-brands fa-github-square fa-2xl"></i></a>
-            <a href="" className="social-btn"><i className="fa-brands fa-twitter-square fa-2xl"></i></a>
+            <a href="https://github.com/WebDevSiDotCom" target="_blank" rel="noreferrer" className="social-btn"><i className="fa-brands fa-github-square fa-2xl"></i></a>
+            <a href="https://twitter.com/webdevsi" className="social-btn"><i className="fa-brands fa-twitter-square fa-2xl"></i></a>
         </div>
     )
 }
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,7 +1,7 @@
-// import React from "react"
+import headshot from './headshot.jpg'
 
 export default function Image() {
     return (
-        <img className="profile--image" src="/headshot.jpg" alt="headshot"/>
+        <img className="profile--image" src={headshot} alt="headshot"/>
     )
 }
